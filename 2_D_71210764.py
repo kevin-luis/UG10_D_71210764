052ff355d9c275df6cbbf03d8c353f0699eaf137
kevin luis <kevin.luis@ti.ukdw.ac.id>
2021-11-18 23:26:12 +0700

    sedikit perubahan

--- 2_D_71210764.py
+++ 2_D_71210764.py
@@ -1,9 +1,11 @@
-#
+#Variabel Input
 import math
 a = float(input("Nilai a :"))
 b = float(input("Nilai b :")) 
 c = float(input("Niali c :"))
 D = (b**2 - (4*a*c))
+
+#Eksekusi
 
 if D == 0 :
     x = -b/(2*a)
